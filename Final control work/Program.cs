e609a02cf8f91be3936e8d5de6232c7eb29245b3
ElenaZueva88 <pusa1799@yandex.ru>
2024-01-08 01:07:33 +0300

    Сохранили Sem_7

---
+++ Final control work/Program.cs
@@ -0,0 +1,59 @@
+﻿// № Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма.При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+
+internal class Program {
+
+static string[] NewArrayString(string[] pull)   
+{
+    string[] chars = new string[pull.Length];   
+        for (int i = 0; i < pull.Length; i++)  
+        {
+            chars[i] = pull[i];                
+        }
+    return chars;                              
+}
+static void ShowArray(string[] array)  
+{
+    Console.Write("The created array" + " ");
+        foreach (string elem in array)
+        {
+            Console.Write(elem + " ");
+        }
+}
+static string[] ModArray(string[] array) 
+{
+    string[] newArray = new string[array.Length];  
+    int count = 0;                                  
+        for (int i = 0; i < array.Length; i++)     
+        {
+            if (array[i].Length <= 3)             
+            {
+                newArray[count] = array[i];        
+                count++;                         
+            }
+        }
+
+    return newArray;                              
+}
+
+private static void Main(string[] args){
+
+Console.WriteLine("Введите строку, разделенную запятыми, в качестве элемента массива");
+    string s = Console.ReadLine();          
+        string[] pull = s.Split(',');        
+                                            
+string[] myArray = NewArrayString(pull);        
+        ShowArray(myArray);
+
+        Console.WriteLine();
+
+string[] newArray = ModArray(myArray);
+    ShowArray(newArray);
+    }
+}
+
+
+
